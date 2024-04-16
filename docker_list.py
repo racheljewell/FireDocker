6ef2c4761940f4737415b94fa72747b6c41c1d3a
TimothyMaraj <timxmaraj@gmail.com>
2024-04-16 00:46:17 -0500

    code updates and modularization

--- docker_list.py
+++ docker_list.py
@@ -1,6 +1,24 @@
 import socket
 
-def docker_list(path, method='GET'):
+
+debug = False
+
+"""
+
+TODO: 
+    ADD PARSER FOR ALL RESPONSES TO SHOW JUST
+    1) Name
+    2) ID
+    3) Maybe some other import info for each response
+
+    4) Prob will have to have a custom parser so that
+       for each end of a response 
+       parse it and remove theese key value pairs
+
+"""
+
+
+def docker_list(path=None, method='GET'):
     # Create a Unix domain socket
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     
@@ -20,12 +38,17 @@ def docker_list(path, method='GET'):
         response_data = b''
         while True:
             chunk = sock.recv(4096)
+
             if not chunk:
                 break
+            temp = response_data
             response_data += chunk
-            print("Received chunk:", response_data.decode())  # Debug print
+
+            if debug: 
+                print("Received chunk:", response_data.decode())  # Debug print
         
             if response_data.endswith(b'0\r\n\r\n'):
+                response_data = temp
                 break
 
         return response_data.decode()
@@ -38,11 +61,5 @@ def docker_list(path, method='GET'):
         sock.close()
 
 
-"""
-
-Route for list command
-
-
-"""
-
-print(docker_list('/v1.40/containers/json?all=1'))
+if debug: 
+    print(docker_list('/v1.40/containers/json?all=1'))
--- docker_requests.py
+++ docker_requests.py
@@ -2,7 +2,8 @@ import json
 import re
 import socket
 debug = False
-def create_container(path, method='GET', data=None):
+
+def create_container(path=None, method='GET', data=None):
     # Create a Unix domain socket
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     try:
@@ -44,51 +45,6 @@ def create_container(path, method='GET', data=None):
         # Close the socket
         sock.close()
 
-"""
-handle chunked encoding
-"""
-
-
-def docker_list(path, method='GET'):
-    # Create a Unix domain socket
-    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    
-    try:
-        sock.connect('/var/run/docker.sock')
-
-        # Prepare the HTTP request
-        request = f'{method} {path} HTTP/1.1\r\n'
-        request += 'Host: unix\r\n'
-        request += 'Content-Type: application/json\r\n'
-        request += '\r\n'  # End of headers
-
-        # Send the request
-        sock.sendall(request.encode())
-        
-        # Receive the response
-        response_data = b''
-        while True:
-            chunk = sock.recv(4096)
-            if not chunk:
-                break
-            response_data += chunk
-            print("Received chunk:", response_data.decode())  # Debug print
-            
-            # Check if we received the terminating chunk '0\r\n\r\n'
-            if response_data.endswith(b'0\r\n\r\n'):
-                break
-        
-        return response_data.decode()
-
-
-    except Exception as e:
-        return str(e)
-    
-    finally: 
-        sock.close()
-
-
-
 def json_parser(strContaingJson): 
     match = re.search(r'\{(.+?)\}', strContaingJson, re.DOTALL)
     if match:
@@ -97,7 +53,7 @@ def json_parser(strContaingJson):
     else:
         print("No JSON data found in the response.")
 
-# Example: Create a container
+# Debug data
 data = {
     "Image": "mysql:latest",
     "Cmd": ["mysqld"],
@@ -110,7 +66,7 @@ data = {
 }
 
 
-
-response_string_container = create_container('/containers/create', method='POST', data=json.dumps(data))
-json_parser(response_string_container)
-#print(docker_list('/v1.40/containers/json?all=1'))
+if debug:
+    response_string_container = create_container('/containers/create', method='POST', data=json.dumps(data))
+    json_parser(response_string_container)
+   
